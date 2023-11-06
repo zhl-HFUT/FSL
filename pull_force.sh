8e96c7e0a930aae45248113750b5588553399162
root <zhl.cn.mail@gmail.com>
2023-11-06 06:08:02 +0000

    update files

--- pull_force.sh
+++ pull_force.sh
@@ -7,7 +7,9 @@ NETWORK_CONFIG_FILE="/etc/network_turbo"
 if [ -e "$NETWORK_CONFIG_FILE" ]; then
     echo "Network configuration file found at $NETWORK_CONFIG_FILE"
     # 在这里添加处理网络配置文件的命令
-    sh /etc/network_turbo
+    source /etc/network_turbo
+    echo $https_proxy
+    echo $http_proxy
 else
     echo "Setting proxy"
     export https_proxy="http://${hostip}:${http_hostport}"
